bfee9b36018b27f446547d033913816c02ad3236
Alex Perez <redflare@gmail.com>
2017-11-06 16:50:00 -0800

    Some stylistic changes, menu order changes, and best-practices submodule update

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var main = function() {
 
 // selects all <a> elements that don't begin with a hash or contain the root, and adds target="_blank"
-  $('a:not([href^="#"], [href^="/"])').attr('target', '_blank');
+  // $('a:not([href^="#"], [href^="/"])').attr('target', '_blank');
 
   $('.anchor-row').click(function() {
 

483715e9f038d82eccfe76d0bb7fe149b643ed58
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-10-08 10:00:17 -0700

    add logic for generating file slugs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from '../components/layout'
+import Layout from '../components/layout';
 
 const IndexPage = () => (
   <Layout>
-    <section id="gtfs-overview" class="jumbotron">
-      <div class="container">
-        <div class="row">
-          <div class="col-xs-12">
+    <section id="gtfs-overview" className="jumbotron">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12">
             <h1>General Transit Feed Specification</h1>
           </div>
-          <div class="col-md-6">
+          <div className="col-md-6">
 
             <p>The General Transit Feed Specification (GTFS) defines an open standard format for exchanging public transportation schedule, geographic and fare information. GTFS “feeds” let public transit agencies publish data in a format that can be consumed and utilized in applications in an interoperable way.</p>
 
             <p>The GTFS format (sometimes referred to as GTFS Static) describes scheduled service. Its companion GTFS Realtime format is used to communicate arrival predictions, vehicle positions, and services advisories. At present, this site is mostly concerned with the GTFS Static format.</p>
 
           </div>
-          <div class="col-md-6">
+          <div className="col-md-6">
 
             <p>GTFS data is available for more than 1350 public transportation providers, and hundreds of applications utilize interoperable GTFS data. This site contains GTFS documentation, best practices, and links to tools.</p>
 
@@ -31,8 +31,8 @@ const IndexPage = () => (
 
 
     <section id="how-do-i-start">
-      <div class="container">
-        <div class="col-xs-12 col-lg-6">
+      <div className="container">
+        <div className="col-xs-12 col-lg-6">
           <h2>How do I start?</h2>
           <ol>
             <li>Take a look at the <a href="">GTFS data examples</a>.</li>

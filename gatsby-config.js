97482502a4512c5576c2287dd69f7a0962471b5d
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-10-09 16:38:42 -0700

    restructure markdown for core reference page

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,12 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography.js'
+      }
+    },
+    {
       resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages/reference`,

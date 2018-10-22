d440ea39b4530b6a7ce06c1ada2f2ada30ff67d1
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-10-22 12:52:14 -0700

    add container for docs sub-header with transition

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import NavDropdown from './dropdown';
+// import NavDropdown from './dropdown';
 import styles from './header.module.css';
 import githubLogo from '../images/github-logo.png';
-import { Dropdown } from 'semantic-ui-react';
+import { Dropdown, Transition } from 'semantic-ui-react';
 // import 'semantic-ui-css/semantic.min.css';
 
 const languageOptions = [{ key: 'English', text: 'English', value: 'English' }];
@@ -28,71 +28,92 @@ const docDropdown = {
   ]
 };
 
-const Header = ({ siteTitle }) => (
-  <div className={styles.container}>
-    <div className={styles.upperRow}>
-      <div className={styles.mobilityData}>
-        <a href="https://www.mobilitydata.org">
-          <h5>Mobility<strong>Data</strong></h5>
-        </a>
-      </div>
-      <div style={{flex: '2', color: '#61b5d9', marginLeft: '25px'}}>
-        Travelers need mobility apps. <strong>Mobility apps need high-quality data</strong>
-      </div>
 
-      <div className={styles.github} onClick={() => window.location = "https://github.com/MobilityData"}>
-        <span>Data specs on GitHub</span>
-        <img src={githubLogo} width="20" height="20"/>
-      </div>
-    </div>
-    <div className={styles.lowerRow}>
-      <div className={styles.logo}>
-        <h1>GTFS</h1>
-      </div>
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    }
+  }
 
-      <div className={styles.links}>
-        <Link
-          to="/"
-          activeClassName={styles.activePage}
-          style={{ textDecoration: 'none' }}
-          >
-            <div className={styles.linkText}>Home</div>
-          </Link>
-          <NavDropdown title="Docs" list={docDropdown.items}></NavDropdown>
-          <Link
-            to="/examples"
-            activeClassName={styles.activePage}
-            >
-              Examples
-            </Link>
-            <Link
-              to="/testing"
-              activeClassName={styles.activePage}
+  toggleDocs() {
+    this.setState(prevState => ({
+      open: !prevState.open
+    }))
+  }
 
-              >
-                Testing Feeds
-              </Link>
-
-
-            </div>
-            <div className={styles.language}>
-              <Dropdown
-                button
-                className='icon'
-                floating
-                labeled
-                icon='world'
-                options={languageOptions}
-                search
-                text='Select Language'
-              />
-            </div>
-
-
-
+  render() {
+    const {open} = this.state;
+    return(
+      <div className={styles.container}>
+        <div className={styles.upperRow}>
+          <div className={styles.mobilityData}>
+            <a href="https://www.mobilitydata.org">
+              <h5>Mobility<strong>Data</strong></h5>
+            </a>
+          </div>
+          <div style={{flex: '2', color: '#61b5d9', marginLeft: '25px'}}>
+            Travelers need mobility apps. <strong>Mobility apps need high-quality data</strong>
           </div>
 
-    </div>
-)
+          <div className={styles.github} onClick={() => window.location = "https://github.com/MobilityData"}>
+            <span>Data specs on GitHub</span>
+            <img src={githubLogo} width="20" height="20"/>
+          </div>
+        </div>
+        <div className={styles.lowerRow}>
+          <div className={styles.logo}>
+            <h1>GTFS</h1>
+          </div>
 
-export default Header
+          <div className={styles.links}>
+            <Link
+              to="/"
+              activeClassName={styles.activePage}
+              style={{ textDecoration: 'none' }}
+              >
+                <div className={styles.linkText}>Home</div>
+              </Link>
+              {/* <NavDropdown title="Docs" list={docDropdown.items}></NavDropdown> */}
+              <div onClick={() => this.toggleDocs()}>Docs</div>
+              <Link
+                to="/examples"
+                activeClassName={styles.activePage}
+                >
+                  Examples
+                </Link>
+                <Link
+                  to="/testing"
+                  activeClassName={styles.activePage}
+
+                  >
+                    Testing Feeds
+                  </Link>
+
+
+                </div>
+                <div className={styles.language}>
+                  <Dropdown
+                    button
+                    className='icon'
+                    floating
+                    labeled
+                    icon='world'
+                    options={languageOptions}
+                    search
+                    text='Select Language'
+                  />
+                </div>
+              </div>
+              <Transition.Group animation='slide down' duration='500'>
+                {open &&
+                  <div className={styles.docsOptions}>
+                  </div>
+                }
+              </Transition.Group>
+        </div>
+      )
+  }
+
+}

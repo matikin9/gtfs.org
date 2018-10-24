364f07d7df24fe8c9037961f73a664d7cad20f21
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-10-23 22:43:23 -0700

    refactor html node sorting

--- src/pages/best-practices.js
+++ src/pages/best-practices.js
@@ -14,9 +14,8 @@ class BestPracticesPage extends React.Component {
     super(props);
     this.data = props.data;
     this.nodeDictionary = {};
+    this.sortedHast = [];
     this.state = {
-      consolidatedHast: this.data.allFile.edges[0].node.childMarkdownRemark.htmlAst,
-      testNode: {},
       parsingComplete: false
     }
     console.log('starter hast node', this.data.allFile.edges[0].node.childMarkdownRemark.htmlAst)
@@ -35,71 +34,33 @@ class BestPracticesPage extends React.Component {
       Object.assign(this.nodeDictionary, pair)
     });
     console.log('complete dictionary', this.nodeDictionary);
-    this.setState({parsingComplete: true});
+    this.sortDictionary()
     // this.consolidateHastNodes();
   }
 
-  consolidateHastNodes() {
-    let newConsolidatedHast = this.nodeDictionary[pageContents.sections[0].slug]; //make first section hast root, all following nodes will be added as children
-    for (let i = 1; i < pageContents.sections.length; i ++) {
-      let thisNode = this.nodeDictionary[pageContents.sections[i].slug];
-      console.log('current node', thisNode);
-      if (thisNode !== undefined) {
-        if (thisNode.type === "root") {
-          newConsolidatedHast.children.concat(thisNode.children);
-        } else {
-          newConsolidatedHast.children.push(thisNode);
-        }
-        if (pageContents.sections[i].children.length > 0) {
-          pageContents.sections[i].children.forEach((child) => {
-            let thisChildNode = this.nodeDictionary[child.slug];
-            if (thisChildNode !== undefined) {
-              if (thisChildNode.type === "root") {
-                newConsolidatedHast.children.concat(thisChildNode.children);
-              } else {
-                newConsolidatedHast.children.push(thisChildNode);
-              }
-            }
-          });
-        }
+  sortDictionary() {
+    pageContents.sections.forEach((section) => {
+      let hast = this.nodeDictionary[section.slug]
+      if (hast !== undefined) this.sortedHast.push(hast);
+      if (section.children.length > 0) {
+        section.children.forEach((child) => {
+          let childHast = this.nodeDictionary[child.slug];
+          if (childHast !== undefined) this.sortedHast.push(childHast);
+        });
       }
-    }
-    console.log('consolidated node', newConsolidatedHast);
-    this.setState({
-      consolidatedHast: newConsolidatedHast,
-      testNode: this.data.allFile.edges[3].node.childMarkdownRemark.htmlAst,
-      parsingComplete: true
     });
-    // console.log(this.consolidatedHast);
-    // console.log(this.data.allFile.edges[0].node.childMarkdownRemark.htmlAst);
+    console.log('sorted hast nodes', this.sortedHast);
+    this.setState({parsingComplete: true});
   }
-
-
-
 
   render() {
-    // const nodes = this.state.consolidatedHast;
-    // console.log('nodes in render', nodes);
-
-      return(
-        <Layout>
-          <h2>Hi</h2>
-          {this.state.parsingComplete && Object.entries(this.nodeDictionary).map(pairArray => renderAst(pairArray[1]))}
-          {/* {renderAst(this.data.allFile.edges[0].node.childMarkdownRemark.htmlAst)} */}
-          {/* {this.state.parsingComplete && renderAst(nodes)} */}
-          {/* {(nodes.children.length === 21) ? renderAst(nodes) : null} */}
-
-        </Layout>
-      )
-    // return(
-    //
-    //   <Layout>
-    //     {/* <div dangerouslySetInnerHTML={{ __html: data.allFile.edges[0].node.childMarkdownRemark.html }}></div> */}
-    //
-    //   </Layout>
-    // )
+    return(
+      <Layout>
+        <h2>Hi</h2>
+        {this.state.parsingComplete && this.sortedHast.map(node => renderAst(node))}
+      </Layout>
+    )
   }
-
 }
 
 export default BestPracticesPage
@@ -108,10 +69,7 @@ export const homeQuery = graphql`
     query {
       allFile(
           filter: {
-            # internal: {mediaType: {eq: "text/markdown"}},
             sourceInstanceName: {eq: "content"},
-            # name: {eq: "best-practices"},
-            # relativePath: {regex: "/en\//"}
           }
         ){
           edges {

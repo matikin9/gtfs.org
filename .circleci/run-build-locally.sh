1930bc8e25da417ffaad956e21ea02cfc51f779e
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-11-09 19:30:25 -0800

    debug

--- .circleci/run-build-locally.sh
+++ .circleci/run-build-locally.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 curl --user ${CIRCLE_TOKEN}: \
     --request POST \
-    --form revision=e2290acea822c60cfc5b9b9c26ecf487b4827a9e\
+    --form revision=22a130415a6d864c1cef03899aee7ec4f0fd48fb\
     --form config=@config.yml \
     --form notify=false \
         https://circleci.com/api/v1.1/project/github/MobilityData/gtfs.org/tree/v2

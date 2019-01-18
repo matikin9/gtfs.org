90812914edf0df9c83e761b39096a71442bcb207
Brendan Nee <brendan@blinktag.com>
2019-01-18 16:31:30 +0100

    Add google analytics to all pages

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,6 +29,15 @@ const Layout = ({ children }) => (
             { name: 'description', content: 'Sample' },
             { name: 'keywords', content: 'sample, something' },
           ]}>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-92157254-1"></script>
+          <script>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'UA-92157254-1');
+            `}
+          </script>
           <html lang="en" />
         </Helmet>
         <div className={styles.headerContainer}>

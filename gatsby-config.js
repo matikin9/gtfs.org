e904c4395a3f7498f162980e648edccf2ea0c756
Brendan Nee <brendan@blinktag.com>
2019-01-24 01:43:45 +0100

    Force trailing slash

--- gatsby-config.js
+++ gatsby-config.js
@@ -130,6 +130,7 @@ module.exports = {
         icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
+    'gatsby-plugin-force-trailing-slashes',
     'gatsby-plugin-offline',
     'gatsby-plugin-catch-links',
     'gatsby-plugin-meta-redirect'

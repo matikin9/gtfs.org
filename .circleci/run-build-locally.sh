037ef56d28bdb558351513ec1f9d71a21b39177f
Brendan Nee <brendan@blinktag.com>
2019-01-24 21:21:04 -0800

    Circleci production deploy

--- .circleci/run-build-locally.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-curl --user ${CIRCLE_TOKEN}: \
-    --request POST \
-    --form revision=22a130415a6d864c1cef03899aee7ec4f0fd48fb\
-    --form config=@config.yml \
-    --form notify=false \
-        https://circleci.com/api/v1.1/project/github/MobilityData/gtfs.org/tree/v2

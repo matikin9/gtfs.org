c7f25bd443bd11691252974dab02ebb08fe6c5eb
Brendan Nee <brendan@blinktag.com>
2019-03-18 13:45:01 -0700

    Remove google translate from 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Layout from '../components/layout'
 import { getPathForLanguage } from '../lib/i18n'
-import { navigate } from 'gatsby'
 import { useTranslation } from 'react-i18next';
 import Footer from '../components/footer';
 
@@ -11,7 +10,6 @@ const NotFoundPage = (props) => {
 
   const renderNotFoundText = () => {
     const englishPath = getPathForLanguage(location.pathname, 'en');
-    const fullEnglishPath = `${location.origin}${englishPath}`;
 
     if (i18n.language === 'en') {
       return (
@@ -23,9 +21,6 @@ const NotFoundPage = (props) => {
           <p className="card-text">{t('Not Found text')} {t(i18n.language)}.</p>
           <p className="card-text">
             <a className="btn btn-primary" href={englishPath}>{t('View in English')}</a>
-          </p>
-          <p className="card-text">
-            <a className="btn btn-primary" href={`http://translate.google.com/translate?hl=&sl=en&tl=${i18n.language}&u=${encodeURIComponent(fullEnglishPath)}`}>{t('You can also use Google Translate')}</a>
           </p>
         </div>
       )

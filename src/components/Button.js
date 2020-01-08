7f9ae849c05983ae54e5cc28a744c02b5d66b16b
Lionel N <lionel@mobilitydata.org>
2020-01-08 09:32:33 -0500

    Revert "Modifies guides page: now  markdowns are used. collapsable"
    This reverts commit 59a29cb7633ed0119b6cf4c2e6aaa16df6d14176.
    
    # Conflicts:
    #	src/pages/en/guides/index.md

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,25 +1,5 @@
 import React from "react";
-import ReactDOM from "react";
-import ReactMarkdown from 'react-markdown';
-
-import ReactMd from 'react-md-file';
-
-// import ReactMd from 'react-md-file';
-// import parseMD from 'parse-md';
-
-// import * as RNFS from 'react-native-fs';
-
-// const fileContents = RNFS.readFile('/Users/lionel/IdeaProjects/gtfs.org/src/pages/en/index.md', 'utf8')
-// const { metadata, content } = parseMD(fileContents)
-
-
-var showdown = require('showdown'),
-    converter = new showdown.Converter(),
-    textToConvert = 'text',
-    convertedText = converter.makeHtml(textToConvert);
-
 export default class Button extends React.Component {
-
 
     handleClick = () => {
         var x = document.getElementById("documentation");
@@ -36,9 +16,6 @@ export default class Button extends React.Component {
         }
 
     }
-
-    // }
-
     render() {
         return <button onClick={this.handleClick}>Generate Specification with selected extensions</button>
     }
